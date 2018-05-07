0bbb12a67275e7002fb52ecfc348fff2d392b134
Maxim Grebennikov <clustermass2@gmail.com>
2018-05-07 15:28:29 -0700

    Finish first controller project

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -1,17 +1,35 @@
 class UsersController < ApplicationController
   def index
-    render plain: "I'm in the index action!"
+    users = User.all
+    render json: users 
 
   end
 
-  def show
-    render json: params
-  end
 
   def create
     user = User.new(params.require(:user).permit(:name, :email))
-    # replace the `user_attributes_here` with the actual attribute keys
-    user.save!
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    user = User.find(params[:id])
     render json: user
   end
+
+  def update
+    user = User.find(params[:id])
+    user.update(params.require(:user).permit(:name, :email))
+    render json: user
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: user
+  end
+
 end
