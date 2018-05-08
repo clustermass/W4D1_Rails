69ed09c8c4423fabf23063785719a01cef5b7a49
Maxim Grebennikov <clustermass2@gmail.com>
2018-05-07 17:00:44 -0700

    Add migrations and models for users, artwork and artwork_shares

---
+++ project/db/migrate/20180507224407_create_artworks.rb
@@ -0,0 +1,35 @@
+class CreateArtworks < ActiveRecord::Migration[5.1]
+  def change
+
+    drop_table :users
+    create_table :users do |t|
+      t.string :username, null: false
+      t.timestamps
+    end
+
+    create_table :artworks do |t|
+      t.string :title, null: false
+      t.string :image_url, null: false
+      t.integer :artist_id, null: false
+      t.timestamps
+    end
+
+    create_table :artworks_shares do |t|
+      t.integer :artwork_id, null: false
+      t.integer :viewer_id, null: false
+      t.timestamps
+    end
+
+    add_index :users, :username, unique: true
+    add_index :artworks, :artist_id
+    add_index :artworks, [:artist_id, :title], unique: true
+    add_index :artworks_shares, [:artwork_id, :viewer_id], unique: true
+    add_index :artworks_shares, :artwork_id
+    add_index :artworks_shares, :viewer_id
+
+
+  end
+
+
+
+end
